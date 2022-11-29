e1e2ca45d1e910a57fd4f269de5e0ade939a8952
Kouvuli <Kouvuli@users.noreply.github.com>
2022-11-29 16:34:28 +0700

    update server base url commit

--- backend/src/main/java/com/example/back/Constants/Constants.java
+++ backend/src/main/java/com/example/back/Constants/Constants.java
@@ -2,7 +2,7 @@ package com.example.back.Constants;
 
 public class Constants {
     public static final String FRONT_BASE_URL="http://localhost:1234";
-    public static final String BACK_BASE_URL="https://vtt-server.herokuapp.com/api/v1";
+    public static final String BACK_BASE_URL="https://midterm-ptudw-production.up.railway.app/api/v1";
 
     public static final String SENDER="ldtam.110201@gmail.com";
 }
